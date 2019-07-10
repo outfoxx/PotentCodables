e1a7ae56e3759336d88037adfe8cd9b13045e12e
Kevin Wooten <kevin@outfoxx.io>
2019-07-10 16:10:00 -0700

    Conform encoders/decoders to capability protocols
    Adds `EncodesToTree`/`DecodesFromTree`, `EncodesToData`/`DecodesFromData` and `EncodesToString`/`DecodesFromString` protocols and conforms all the existing implementations to them.

---
+++ Sources/EncoderSupport.swift
@@ -0,0 +1,57 @@
+//
+//  EncoderSupport.swift
+//  PotentCodables
+//
+//  Copyright © 2019 Outfox, inc.
+//
+//
+//  Distributed under the MIT License, See LICENSE for details.
+//
+
+import Foundation
+
+
+/// All encoders support encoding to their native values.
+///
+public protocol EncodesToTree {
+
+  associatedtype Value: PotentCodables.Value
+
+  /// Encodes the value  to a native value.
+  ///
+  /// - Parameters:
+  ///   - value: Value to encode
+  /// - Returns: Encoded value tree.
+  ///
+  func encodeTree<T: Encodable>(_ value: T) throws -> Value
+
+}
+
+/// Encoders that target binary formats support encoding to data.
+///
+public protocol EncodesToData: EncodesToTree {
+
+  /// Encodes the value to binary data.
+  ///
+  /// - Parameters:
+  ///   - value: Value to encode
+  /// - Returns: Encoded binary data.
+  ///
+  func encode<T: Encodable>(_ value: T) throws -> Data
+
+}
+
+/// Encoders that target text formats support encoding to strings
+/// and encoding to data using a default string encoding.
+///
+public protocol EncodesToString: EncodesToData {
+
+  /// Encodes the encodable `value` to a string.
+  ///
+  /// - Parameters:
+  ///   - value: Value to encode
+  /// - Returns: Encoded text.
+  ///
+  func encodeString<T: Encodable>(_ value: T) throws -> String
+
+}
