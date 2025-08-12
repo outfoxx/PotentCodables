10906713acceb1ed631ecb8613997a410fb3d077
Kevin Wooten <kevin@wooten.com>
2025-08-11 20:43:41 -0700

    Update deps to lates for Swift 6 compatibility

--- Package.swift
+++ Package.swift
@@ -43,9 +43,9 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
+    .package(url: "https://github.com/attaswift/BigInt.git", from: "5.7.0"),
     .package(url: "https://github.com/SusanDoggie/Float16.git", from: "1.1.1"),
-    .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.4"),
+    .package(url: "https://github.com/apple/swift-collections.git", from: "1.2.1"),
     .package(url: "https://github.com/sharplet/Regex.git", from: "2.1.1")
   ],
   targets: [
@@ -112,7 +112,7 @@ let package = Package(
       dependencies: ["PotentCodables", "PotentJSON", "PotentCBOR", "PotentASN1", "PotentYAML"],
       path: "./Tests"
     )
-  ]
+  ],
 )
 
 #if swift(>=5.6)
